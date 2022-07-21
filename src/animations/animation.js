b3a378a02b1b5b274e189f1590c26ece4ddb10f8
Dmitry <d.volkozubov@brightlab.me>
2022-07-21 19:41:22 +0300

    intersection observer fix

--- src/animations/animation.js
+++ src/animations/animation.js
@@ -2,19 +2,12 @@ const blockClassName = ".main-content__block";
 
 const observerOptions = {
   root: null,
-  rootMargin: "20px",
-  threshold: 1.0,
-
-  trackVisibility: true,
-  delay: 100,
+  rootMargin: "20px 0px 20px 0px",
 };
 
 const checkInView = (entries) => {
-  console.log("entries: ", entries);
-  console.log("her");
   entries.map((entry) => {
-    console.log("entry: ", entry);
-    if (entry.isVisible) {
+    if (entry.intersectionRatio !== 0) {
       entry.target.className = blockClassName + " red";
     } else {
       entry.target.className = blockClassName + " blue";
